98fce3b44bb4d2b94ca7a42f7904dfa9b17cdf76
Jareer Hassan <jareerhassan8@gmail.com>
2026-05-12 10:03:44 +0500

    hd

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  async rewrites() {
+    return [];
+  },
+
+  trailingSlash: false,
 };
 
 export default nextConfig;
